5233be3264cfa74f311e41533556448588875977
HilRAM210 <baimhilal2006@gmail.com>
2025-03-13 12:04:56 +0700

    Add files via upload

---
+++ SallaryWithLoopingFor.py
@@ -0,0 +1,27 @@
+status_list = ["Tetap", "Honor"]
+golongan_list = ["A", "B", "C"]
+
+print("\n\t\t\t\t\tDAFTAR GAJI")
+print("+--------+-------+----------+---+------+---------+-------+--------+------------+----------+")
+
+for status in status_list:
+    for golongan in golongan_list:
+        gaji = 0
+        bonus = 0
+        
+        if status == "Tetap":
+            gaji = 1500000
+        elif status == "Honor":
+            gaji = 1000000
+        
+        if golongan == "A":
+            bonus = 200000 if status == "Tetap" else 150000
+        elif golongan == "B":
+            bonus = 400000 if status == "Tetap" else 275000
+        elif golongan == "C":
+            bonus = 550000 if status == "Tetap" else 480000
+        
+        total_gaji = gaji + bonus
+        
+        print("| Status |", status, "| Golongan |", golongan, "| Gaji |", gaji, "| Bonus |", bonus, "| Total Gaji |", total_gaji, " |")
+        print("+--------+-------+----------+---+------+---------+-------+--------+------------+----------+")
